33cee27ae82daa5ef4b4bfe8c3cba4e28c2a9696
Yuan Hao Chiang <yuanhao.chiang@edmodo.com>
2017-06-22 08:30:36 -0600

    Removing token and reply with one form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  #removed so that anyone can send a request
+  #protect_from_forgery with: :exception
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def gravatar_url(email, size)
     gravatar_id = Digest::MD5::hexdigest(email).downcase
-    default_url = image_url("images/mf_avatar.png")
+    default_url = "https://mindfulflexibility.com/layout/mf_avatar.png" #image_url("mf_avatar.png")
     url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
   end
     
