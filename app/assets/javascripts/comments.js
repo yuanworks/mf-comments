9573014ab543ca18a1373f114dfdaf488d5be1d2
Yuan Hao Chiang <yuanhao.chiang@edmodo.com>
2017-06-21 20:16:06 -0600

    Reply button

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,11 +1,27 @@
 
-window.onload = initializeComments;
+window.onload = initializeComments
 
 function initializeComments() {
     setReplyLinks();
-    console.log("finished initialize");
+    console.log("finished initialize")
 }
 
 function setReplyLinks() {
+    var replyLinks = document.getElementsByClassName("reply")
+    console.log("caca")
+    for (i = 0; replyLinks.length; i++) {
+        replyLinks[i].addEventListener("click", moveReplyBox)
+    }    
+}
+
+function moveReplyBox() {
+    console.log(this)
     
+    //
+    this.parentElement.appendChild(document.getElementById("mfc-comment-box"));
+    
+    var form = document.getElementById("new_comment");
+    var replyID = this.id.split('-')[2];
+	
+    form.action = "/comments/" + replyID + "/comments";
 }
