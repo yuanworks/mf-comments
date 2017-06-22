0f27db4cc47297c849387b30e29e52202d7459eb
Yuan Hao Chiang <yuanhao.chiang@edmodo.com>
2017-06-22 08:14:59 -0600

    mf avatar default image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   def gravatar_url(email, size)
     gravatar_id = Digest::MD5::hexdigest(email).downcase
-    default_url = "http://mysite.com/myavatar.png"
+    default_url = image_url("images/mf_avatar.png")
     url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI::escape(default_url)}"
   end
     
