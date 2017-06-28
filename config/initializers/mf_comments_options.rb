0adb3af399318e2fd89ede585d5d5b74dee8959d
Yuan Hao Chiang <yuanhao.chiang@edmodo.com>
2017-06-28 11:36:10 -0600

    Adding Config options (required rails restart)

---
+++ config/initializers/mf_comments_options.rb
@@ -0,0 +1,6 @@
+# MF-Comments Configuration Options
+# NOTE: Changing this values requires restarting the rails s !
+# MyApp::Application.config.x.<KEY_NAME> = <VALUE_NAME>
+MfComments::Application.config.x.show_comment_date = false
+# Same as:
+# Rails.application.config.x.show_comment_date = false
